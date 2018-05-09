27b1f8b00f8cb716642603ad0d28c59e1cf7cb6a
mikelebike <34459839+wallgrenMike@users.noreply.github.com>
2018-05-09 15:56:59 +0200

    added improved, stochastic model
    stochastic inspirational generator with a variety of quotes

---
+++ inspirational_quotes_master.m
@@ -0,0 +1,25 @@
+notprinted = 1;
+while(notprinted)
+    if(rand<0.1)
+        notprinted = 0;
+        disp("you can do this!");
+    elseif(rand<0.1)
+        notprinted = 0;
+        disp("if you do just a little everyday, that puts so you much further ahead of the competition");
+    elseif(rand<0.1)
+        notprinted = 0;
+        disp("rest is for the wicked");
+    elseif(rand<0.1)
+        notprinted = 0;
+        disp("quiters no winners!")
+    elseif(rand<0.1)
+        notprinted = 0;
+        disp("pain is just the body leaving weakness, or however it was again...")
+        elseif(rand<0.1)
+        notprinted = 0;
+        disp("YOU are amazing!")
+        elseif(rand<0.1)
+        notprinted = 0;
+        disp("A beautiful day for a beautiful girl/boy")
+    end
+end
